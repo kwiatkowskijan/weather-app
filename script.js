2794949bbc632628c4c8cbeb80f029814118a881
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2023-06-13 17:30:20 +0200

    update

--- script.js
+++ script.js
@@ -6,39 +6,74 @@ async function checkWeather() {
   const response = await fetch(apiUrl + `&appid=${apiKey}` + `&q=${location}`);
   var data = await response.json();
 
-  document.getElementById("weather-result").style.display = "flex";
-  
-  // document.getElementById("location").textContent = data.name + ", " + data.sys.country;
-  document.getElementById("temp").textContent = data.main.temp + "°C";
-  document.getElementById("description").textContent = data.weather[0].description;
-  document.getElementById("wind-speed").textContent = data.wind.speed + " m/s";
-  document.getElementById("humidity-percantage").textContent = data.main.humidity + " %";
+  if (response.status == 404) {
+    document.getElementById("blad").style.display = "block";
+    document.getElementById("weather-result").style.display = "none";
+  } else {
+    document.getElementById("blad").style.display = "none";
+    document.getElementById("weather-result").style.display = "flex";
+    document.getElementById("temp").textContent = data.main.temp + "°C";
+    document.getElementById("description").textContent = data.weather[0].description;
+    document.getElementById("wind-speed").textContent = data.wind.speed + " m/s";
+    document.getElementById("humidity-percantage").textContent = data.main.humidity + " %";
 
-  const weatherIcon = document.getElementById("weatherIcon");
-  const description = data.weather[0].description;
+    const weatherIcon = document.getElementById("weatherIcon");
+    const main = data.weather[0].main;
 
-  const weatherIcons = {
-    "clear sky": "media/icons/clear-day.svg",
-    "scattered clouds": "media/icons/cloudy.svg",
-    "few clouds": "media/icons/overcast.svg",
-    "broken clouds": "media/icons/partly-cloudy-day.svg",
-    "shower rain": "media/icons/rain.svg",
-    "rain": "media/icons/partly-cloudy-day-rain.svg",
-    "thunderstorm": "media/icons/thunderstorms-extreme.svg",
-    "snow": "media/icons/snow.svg",
-    "mist": "media/icons/fog.svg"
-  };
+    const weatherIcons = {
+      "Clear": "media/icons/clear-day.svg",
+      "Clouds": "media/icons/cloudy.svg",
+      "Rain": "media/icons/rain.svg",
+      "Drizzle": "media/icons/partly-cloudy-day-rain.svg",
+      "Thunderstorm": "media/icons/thunderstorms-extreme.svg",
+      "Snow": "media/icons/snow.svg",
+      "Atmosphere": "media/icons/fog.svg"
+    };
 
-  weatherIcon.src = weatherIcons[description];
+    weatherIcon.src = weatherIcons[main];
 
-  const windSpeed = data.wind.speed;
+    //#############################################################################
 
-  if (windSpeed >= 0 && windSpeed <= 5) {
+    const windIcon = document.getElementById("windIcon");
+    const windSpeed = data.wind.speed;
 
-  } else if (windSpeed >= 5.01 && windSpeed <= 10) {
+    const windIcons = {
+      "low": "media/icons/windsock-weak.svg",
+      "medium": "media/icons/wind.svg",
+      "high": "media/icons/wind-alert.svg"
+    };
 
+    let selectedWindIcon = windIcons.medium;
+
+    if (windSpeed >= 14) {
+      selectedWindIcon = windIcons.high;
+    } else if (windSpeed <= 8) {
+      selectedWindIcon = windIcons.low;
+    }
+
+    windIcon.src = selectedWindIcon;
+
+    //#############################################################################
+
+    const humidityIcon = document.getElementById("humidityIcon");
+    const humidity = data.main.humidity;
+
+    const humidityIcons = {
+      "low": "media/icons/pollen.svg",
+      "medium": "media/icons/raindrop.svg",
+      "high": "media/icons/raindrops.svg"
+    };
+
+    let selectedHumidityIcon = humidityIcons.medium;
+
+    if (humidity >= 60) {
+      selectedHumidityIcon = humidityIcons.high;
+    } else if (humidity <= 40) {
+      selectedHumidityIcon = humidityIcons.low;
+    }
+
+    humidityIcon.src = selectedHumidityIcon;
   }
-
 }
 
 document.getElementById("searchCityForm").addEventListener("submit", function (event) {
